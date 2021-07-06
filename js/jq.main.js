d2c92d572bbfc97649a7df8c5194d8f0a57c77d7
ART_mj1 <mjmjpc@naver.com>
2021-07-06 10:05:34 +0900

    07/06 mammoth renewal 진행 중

---
+++ js/jq.main.js
@@ -0,0 +1,23 @@
+$(function(){
+ // Mega Box Menu Show When Hovering Navigation
+ $(".topnav, .hidden_sub").on("mouseenter",function(){
+  $(".hidden_sub").css({display:"block"});
+  $(".topnav li a").css({color:"#000"});
+  $(".hidden_sub").stop().animate({height:"485px"},300);
+
+ ;})
+ $(".topnav, .hidden_sub").on("mouseleave",function(){
+   $(".hidden_sub").stop().animate({height:0},300,function(){
+    $(".topnav li a").css({color:"#fff"});
+    $(".hidden_sub").css({display:"none"});
+   });
+ });
+ //Sub Mega Box Menu Hide and Show
+ $(".topnav li").on("mouseenter",function(){
+  const i = $(this).index();
+  $(".subnav_box").hide();
+  $(".subnav_box").eq(i).show();
+ })
+});
+
+// $(document).ready(function(){});
